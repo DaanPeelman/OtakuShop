bff35f6ca7509799e70fdc37ea9bc1c5f942687d
Daan <daanpeelman@gmail.com>
2014-09-01 14:28:11 +0200

    Transactional bijgevoegd aan UitgeverServiceImpl

--- src/main/java/be/otakushop/services/UitgeverServiceImpl.java
+++ src/main/java/be/otakushop/services/UitgeverServiceImpl.java
@@ -2,11 +2,13 @@ package be.otakushop.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import be.otakushop.dao.UitgeverDAO;
 import be.otakushop.entities.Uitgever;
 
 @Service
+@Transactional(readOnly = true)
 class UitgeverServiceImpl implements UitgeverService {
 	private final UitgeverDAO uitgeverDAO;
 	
