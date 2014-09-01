f30092144b35f142eedc97df4017089cfd393048
Daan <daanpeelman@gmail.com>
2014-09-01 16:22:18 +0200

    Producten op productenpagina worden nu op naam gesorteerd

--- src/main/java/be/otakushop/services/ProductServiceImpl.java
+++ src/main/java/be/otakushop/services/ProductServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -43,7 +44,7 @@ class ProductServiceImpl implements ProductService {
 	
 	@Override
 	public Iterable<Product> findAll() {
-		return productDAO.findAll();
+		return productDAO.findAll(new Sort(Sort.Direction.ASC, "titel"));
 	}
 	
 	@Override
