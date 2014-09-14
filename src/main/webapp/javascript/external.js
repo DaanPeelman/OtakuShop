fc784b38c796dbab1fb97705acb37b0800a80830
Daan <daanpeelman@gmail.com>
2014-09-14 15:13:33 +0200

    Aanpassing JQuery

--- src/main/webapp/javascript/external.js
+++ src/main/webapp/javascript/external.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
 	$('a[rel="blank"]').attr("target", "_blank");
+	
+	$('.product_rij').hover(
+		function() {
+			$(this).find('h3').css('text-decoration', 'underline');
+		},
+		function() {
+			$(this).find('h3').css('text-decoration', 'none');
+		}
+	);
+	$('.product_rij').click(function() {
+		
+		var nProductId = $(this).find('#productId').val();
+		window.location.replace("/" + nProductId);
+	});
 });
